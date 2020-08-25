eeb25b5107cc6aae2cf4fa877fe5cf27d6eabb8f
hani <haniyousfi@gmail.com>
2020-08-25 01:03:40 +0100

    Edit settings.py

--- Dog_Breed_Classifier_API/settings.py
+++ Dog_Breed_Classifier_API/settings.py
@@ -132,10 +132,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-STATICFILES_DIRS = ()
+STATICFILES_DIRS = os.path.join(os.path.dirname(BASE_DIR), "static")
